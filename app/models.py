93d4ffe1d21c71ddec873405ccb37e4cfde90b37
kyosuke yanai <wyainaxio4s8s@gmail.com>
2021-12-12 02:05:09 +0900

    add TopPage

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name="タイトル", max_length=50)
+    text = models.TextField(verbose_name="本文", max_length=1000)
+    created = models.DateField(verbose_name="投稿日", auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'app/index.html'
+    model = Post
+    ordering = '-id'
