68b2b3bb4f01045fcf22c909715bd2950c95931c
kyosuke yanai <wyainaxio4s8s@gmail.com>
2021-12-12 11:21:44 +0900

    add DetailPage

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(),name="index"),
+    path('detail/<int:pk>', views.BlogDetailView.as_view(),name="blog_detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class IndexView(ListView):
     template_name = 'app/index.html'
     model = Post
     ordering = '-id'
+
+class BlogDetailView(DetailView):
+    template_name = 'app/blog_detail.html'
+    model = Post
