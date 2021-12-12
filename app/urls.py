3d3cf749355e1fdaf7f79136fddd38577f97b775
kyosuke yanai <wyainaxio4s8s@gmail.com>
2021-12-12 11:38:39 +0900

    add CreateBlog

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(),name="index"),
-    path('detail/<int:pk>', views.BlogDetailView.as_view(),name="blog_detail"),
+    path('detail/<int:pk>/', views.BlogDetailView.as_view(),name="blog_detail"),
+    path('create/', views.BlogCreateView.as_view(),name="blog_create"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 class IndexView(ListView):
     template_name = 'app/index.html'
@@ -10,3 +12,8 @@ class IndexView(ListView):
 class BlogDetailView(DetailView):
     template_name = 'app/blog_detail.html'
     model = Post
+
+class BlogCreateView(CreateView):
+    template_name = 'app/blog_create.html'
+    form_class = PostForm
+    success_url = reverse_lazy('index')
