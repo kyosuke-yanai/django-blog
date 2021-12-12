4e4b5affd88ad676a747439bdfc23be62c4ae8f8
kyosuke yanai <wyainaxio4s8s@gmail.com>
2021-12-12 10:55:32 +0900

    fix TopPage

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=50)
     text = models.TextField(verbose_name="本文", max_length=1000)
-    created = models.DateField(verbose_name="投稿日", auto_now_add=True)
+    created = models.DateTimeField(verbose_name="投稿日", auto_now_add=True)
 
     def __str__(self):
         return self.title
