fd7a8f1d686cc8ac6f07b983c28941a523e6dd42
kyosuke yanai <wyainaxio4s8s@gmail.com>
2021-12-12 12:17:22 +0900

    add EditBlog DeleteBlog

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(),name="index"),
     path('detail/<int:pk>/', views.BlogDetailView.as_view(),name="blog_detail"),
     path('create/', views.BlogCreateView.as_view(),name="blog_create"),
+    path('edit/<int:pk>/', views.BlogEditView.as_view(),name="blog_edit"),
+    path('delete/<int:pk>/', views.BlogDeleteView.as_view(),name="blog_delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(ListView):
     template_name = 'app/index.html'
@@ -16,4 +17,15 @@ class BlogDetailView(DetailView):
 class BlogCreateView(CreateView):
     template_name = 'app/blog_create.html'
     form_class = PostForm
+    success_url = reverse_lazy('index')
+
+class BlogEditView(LoginRequiredMixin, UpdateView):
+    template_name = 'app/blog_edit.html'
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('index')
+
+class BlogDeleteView(DeleteView):
+    template_name = 'app/blog_delete.html'
+    model = Post
     success_url = reverse_lazy('index')
