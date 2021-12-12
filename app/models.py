4187d7974fa43606b8a82628aec3d3fd49f1fe50
kyosuke yanai <wyainaxio4s8s@gmail.com>
2021-12-12 22:59:28 +0900

    add comment

--- app/models.py
+++ app/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    comment_by = models.ForeignKey(Post, verbose_name="対象ブログ", on_delete=models.CASCADE)
+    author = models.CharField(verbose_name="投稿者", max_length=50, default="名無しのマッチョ")
+    text = models.TextField(verbose_name="コメント", max_length=500)
+    created = models.DateTimeField(verbose_name="投稿日", auto_now_add=True)
+
+    def __str__(self):
+        return self.author
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
-from .forms import PostForm
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(ListView):
@@ -10,9 +10,23 @@ class IndexView(ListView):
     model = Post
     ordering = '-id'
 
-class BlogDetailView(DetailView):
+class BlogDetailView(DetailView, FormView):
     template_name = 'app/blog_detail.html'
     model = Post
+    form_class = CommentForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_list'] = Comment.objects.filter(comment_by=Post.objects.get(id=self.kwargs['pk']))
+        return context
+
+    def form_valid(self, form):
+        form.instance.comment_by = Post.objects.get(id=self.kwargs['pk'])
+        form.save()
+        return super().form_valid(form)
+        
+    def get_success_url(self):
+        return reverse('blog_detail', kwargs={'pk': self.kwargs['pk']})
 
 class BlogCreateView(LoginRequiredMixin, CreateView):
     template_name = 'app/blog_create.html'
